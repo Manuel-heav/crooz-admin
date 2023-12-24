522f11a235c9b1fb6351d4e78fcc5224ed1181be
manuel-heav <manuelheav32@gmail.com>
2023-12-24 17:27:52 +0300

    working on sidebar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
-import Header from './Header'
+import SideBar from './Sidebar'
 
 const Home = () => {
   return (
     <div>
-      <Header />
-      <h1>Home</h1>
-      <p className='text-white bg-blue-500'>Home page</p>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <SideBar />
     </div>
   )
 }
